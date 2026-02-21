2d2f33f0e6baecaf44cc00fc0e41091ba1dfc8d7
BUGS-HU1278-OYLC <omar.lozano@binsights.co>
2026-02-21 16:22:55 -0500

    feat: add reusable task card component

---
+++ angular-test/src/app/features/tasks/components/task-card/task-card.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Task } from '../../models/task.model';
+
+@Component({
+  selector: 'app-task-card',
+  standalone: true,
+  imports: [CommonModule, RouterLink, DatePipe],
+  templateUrl: './task-card.component.html',
+  styleUrl: './task-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TaskCardComponent {
+  @Input({ required: true }) task!: Task;
+
+  @Output() complete = new EventEmitter<Task>();
+  @Output() remove = new EventEmitter<number>();
+
+  get currentState(): string {
+    const last = this.task.stateHistory?.[this.task.stateHistory.length - 1];
+    return last?.state ?? 'unknown';
+  }
+
+  onComplete() {
+    this.complete.emit(this.task);
+  }
+
+  onRemove() {
+    this.remove.emit(this.task.id);
+  }
+
+  trackByIndex = (i: number) => i;
+}
