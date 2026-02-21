899cec4774f733930f0058532244945afe40bade
BUGS-HU1278-OYLC <omar.lozano@binsights.co>
2026-02-21 16:12:49 -0500

    chore: add base HTTP error interceptor

--- angular-test/src/app/core/interceptors/http-error.interceptor.ts
+++ angular-test/src/app/core/interceptors/http-error.interceptor.ts
@@ -3,10 +3,6 @@ import { throwError } from 'rxjs';
 
 export const httpErrorInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req).pipe(
-    // podríamos mapear aquí mensajes por status si quieres
-    // dejo simple y robusto
-    // catchError debe importarse si lo activamos con lógica
-    // lo configuramos cuando integremos UI de error
     (source) => source
   );
 };
